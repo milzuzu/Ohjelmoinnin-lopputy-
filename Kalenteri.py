aaadac858033eecdb499e2728b61fd89776a7e31
Elmeri Kurkela <elmeri.kurkela@edu.turkuamk.fi>
2025-03-29 16:34:38 +0200

    Lisätty def valikko(kesken) ja def tapahtuman lisäys

--- Kalenteri.py
+++ Kalenteri.py
@@ -1,6 +1,33 @@
 import calendar
 from tabulate import tabulate
 
+# Valikko, käyttäjä voi valita haluamansa toiminnon
+def valikko():
+    filename = "kalenteri.txt" # Määritellään oikea tiedoston nimi
+    # Looppi, jotta ohjelma pyörii, kunnes käyttäjä valitsee lopettaa ///KESKEN!
+    while True:
+        # Printit
+        print("Tervetuloa käyttämään kalenteria! ")
+        print("\nValitse toiminto:")
+        print("1. Tarkastele kalenteria")
+        print("2. Lisää tapahtuma")
+        print("3. Poista tapahtuma")
+        print("4. Muokkaa tapahtumaa")
+        print("5. Lopeta ohjelma")
+        valinta = input("Valitse toiminto (1-5):") # Käyttäjä valitsee numeron, joka vastaa toimintoa
+
+        if valinta == 1:
+            vuosi = int(input("Anna vuosi: "))
+            kuukausi = int(input("Anna kuukausi: "))
+            tapahtumat = [1, 10, 20] # Paikanpitäjä tapahtumat // Tähän koodi, joka avaa tekstitiedoston, jossa tallennetut tapahtumat
+            tarkastelu(vuosi, kuukausi, tapahtumat)
+        elif valinta == 2:
+            tapahtuman_lisäys(vuosi, kuukausi, tapahtumat)
+        else:
+            print("Valitse numero (1-5)")
+        
+
+
 
 # Tulostaa kuukausikalenterin ja merkitsee tapahtumapäivät tähdellä (*)
 def tarkastelu(vuosi, kuukausi, tapahtumat):
@@ -27,13 +54,30 @@ def tarkastelu(vuosi, kuukausi, tapahtumat):
     print(f"\n{calendar.month_name[kuukausi]} {vuosi}".center(30))
     print(tabulate(muokattu_kuukausi, headers=otsikot, tablefmt="grid"))
 
+# Lisää tapahtuman tiedostoon kalenteri.txt
+def tapahtuman_lisäys(vuosi, kuukausi, paiva):
+    filename = "kalenteri.txt"
+    try:
+        with open(filename, "a") as file: # Avataan tiedosto muokkaus modessa
+            print("-" * 20) # Printataan raamia
+            print("Tapahtuman lisäys -- esim. 2025 3 07")
+            tietue = input("\nKerro vuosi, kuukausi ja päivä: ")
+            print("-" * 20) # Printataan raamia
+            file.write(tietue + "\n") # Kirjoitetaan tiedostoon haluttu tietue + rivinvaihto
+            print("\nTapahtuma tallennettu onnistuneesti. ")
+    except FileNotFoundError:
+        print("Tapahtumien haussa kävi virhe") # Error printti, jos tiedostoa ei löydy
+    except Exception as e:
+        print(f"Odottamaton virhe: {e}") # Error printti muihin virheisiin
+    except OSError as e:
+        print(f"Virhe tiedostoon kirjoittamisessa: {e}") # Virheilmoitus muille tiedostovirheille
+
+
+
+
 def main():
     
-    vuosi = int(input("Valitse vuosi: "))
-    kuukausi = int(input("Valitse kuukausi: "))
-    tapahtumat = [5, 10, 17]
+    valikko()
 
-    tarkastelu(vuosi, kuukausi, tapahtumat)
 
-if __name__ == "__main__":  
     main()
